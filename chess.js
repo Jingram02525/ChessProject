685c99bed8ee6827b66c606d37bc3ae487eb624f
Jingram02525 <86169152+Jingram02525@users.noreply.github.com>
2023-11-14 20:45:17 -0800

    Update chess.js

--- chess.js
+++ chess.js
@@ -3,11 +3,57 @@ class ChessPiece {
     constructor(color, type) {
       this.color = color;
       this.type = type;
+    }  
+    
+    isValidMove(sourceRow, sourceCol, destRow, destCol, board) {
+      throw new Error("Subclasses must implement isValidMove method");
+    }
+  }
+   
+  class Pawn extends ChessPiece {
+    constructor(color) {
+      super(color, 'pawn');
+      this.hasMoved = false; // Keep track of whether the pawn has moved
     }
   
-    // Implement piece-specific move validation logic here
+    isValidMove(sourceRow, sourceCol, destRow, destCol, board) {
+      const direction = this.color === 'white' ? 1 : -1; // Define the direction based on the pawn's color
+  
+      // Basic forward move (one square)
+      if (destCol === sourceCol && destRow === sourceRow + direction && board[destRow][destCol] === null) {
+        this.hasMoved = true;
+        return true;
+      }
+  
+      // Initial two-square move
+      if (
+        !this.hasMoved &&
+        destCol === sourceCol &&
+        destRow === sourceRow + 2 * direction &&
+        board[sourceRow + direction][destCol] === null &&
+        board[destRow][destCol] === null
+      ) {
+        this.hasMoved = true;
+        return true;
+      }
+  
+      // Diagonal capture
+      if (
+        Math.abs(destCol - sourceCol) === 1 &&
+        destRow === sourceRow + direction &&
+        board[destRow][destCol] !== null &&
+        board[destRow][destCol].color !== this.color
+      ) {
+        this.hasMoved = true;
+        return true;
+      }
+  
+      // Invalid move
+      return false;
+    }
   }
   
+
   // ChessBoard class representing the chessboard
   class ChessBoard {
     constructor() {
@@ -17,12 +63,38 @@ class ChessPiece {
   
     // Add pieces to the initial positions
     initialize() {
-      // Implement the initial piece setup here
+        // Implement the initial piece setup here
+        // Example: Set up pawns
+        for (let col = 0; col < 8; col++) {
+            this.grid[1][col] = new ChessPiece('white', 'pawn');
+            this.grid[6][col] = new ChessPiece('black', 'pawn');
+        }
+        // Add other pieces (rooks, knights, bishops, kings, and queens) in their initial positions
     }
   
     // Move a piece on the board
     movePiece(sourceRow, sourceCol, destRow, destCol) {
-      // Implement piece movement logic here
+        // Implement piece movement logic here
+        const sourcePiece = this.grid[sourceRow][sourceCol];
+        const destPiece = this.grid[destRow][destCol];
+    
+        // Check if the move is valid for the source piece
+        if (sourcePiece.isValidMove(sourceRow, sourceCol, destRow, destCol, this.grid)) {
+            // Perform the move
+            this.grid[destRow][destCol] = sourcePiece;
+            this.grid[sourceRow][sourceCol] = null;
+    
+            // Check for check and checkmate after the move
+            if (this.checkForCheckmate()) {
+                // Handle checkmate logic
+            } else if (this.checkForCheck()) {
+                // Handle check logic
+            }
+    
+            return true; // Move successful
+        } else {
+            return false; // Invalid move
+        }
     }
   
     // Check for check and checkmate
@@ -37,15 +109,25 @@ class ChessPiece {
   
   // Player class representing a human player
   class Player {
-    constructor(name, color) {
+    constructor(name, color, game) {
       this.name = name;
       this.color = color;
+      this.game = game; // Reference to the chess game
     }
   
     // Make a move on the board
     makeMove(sourceRow, sourceCol, destRow, destCol) {
-      // Implement player move logic here
-    }
+        // Implement player move logic here
+        // Check if it's the player's turn and if the move is within the player's color
+        if (this.color === this.game.currentPlayer.color) {
+          // Make the move on the board
+          this.game.board.movePiece(sourceRow, sourceCol, destRow, destCol);
+          // Switch to the next player's turn
+          this.game.switchTurn();
+        } else {
+          console.log("It's not your turn.");
+        }
+      }
   }
   
   // ChessGame class representing the main game
@@ -70,25 +152,27 @@ class ChessPiece {
     // Play the game
     play() {
         while (!this.isGameOver()) {
-        // Display the current state of the board and whose turn it is
-        this.displayBoard();
-        console.log(`It's ${this.currentPlayer.name}'s turn.`);
-    
-        // Get the player's move input
-        const move = this.getPlayerMove();
-    
-        // Attempt to make the move on the board
-        if (this.board.movePiece(move.sourceRow, move.sourceCol, move.destRow, move.destCol)) {
+          // ... (rest of the loop)
+          const move = this.getPlayerMove();
+      
+          // Exit the loop if the player wants to quit
+          if (move.quit) {
+            console.log('Game Over. Player decided to quit.');
+            break;
+          }
+      
+          // Attempt to make the move on the board
+          if (this.board.movePiece(move.sourceRow, move.sourceCol, move.destRow, move.destCol)) {
             // Switch to the next player's turn
             this.switchTurn();
-        } else {
+          } else {
             console.log('Invalid move. Try again.');
+          }
         }
-        }
-    
+      
         // The game is over, display the result
         this.displayGameResult();
-    }
+      }
     
     isGameOver() {
         return this.board.checkForCheckmate() || this.board.checkForStalemate();
@@ -96,14 +180,32 @@ class ChessPiece {
     
     displayBoard() {
         // Implement code to display the current state of the board
+        // You can use console.log to print the grid
+        for (let row = 0; row < 8; row++) {
+            console.log(this.board.grid[row].map(piece => (piece ? piece.type[0] : '.')).join(' '));
+        }
     }
     
     getPlayerMove() {
         // Implement code to get the player's move input
+        // You can use a library like readline-sync or prompt-sync to get input from the console
+        // Example using readline-sync:
+        const readlineSync = require('readline-sync');
+        const sourceRow = parseInt(readlineSync.question('Enter source row: '));
+        const sourceCol = parseInt(readlineSync.question('Enter source column: '));
+        const destRow = parseInt(readlineSync.question('Enter destination row: '));
+        const destCol = parseInt(readlineSync.question('Enter destination column: '));
+    
+        return { sourceRow, sourceCol, destRow, destCol };
     }
     
     displayGameResult() {
         // Implement code to display the game result (checkmate, stalemate, etc.)
+        if (this.board.checkForCheckmate()) {
+            console.log(`Checkmate! ${this.currentPlayer.name} wins.`);
+        } else {
+            console.log('Stalemate! The game is a draw.');
+        }
     }
     }
   
